c90998a248c63732429f057071656ba41d991d08
Torstein I. Bø <torsteinibo@gmail.com>
2018-03-18 19:28:35 +0100

    Implemented extraction of data

--- test/test_yrextract.py
+++ test/test_yrextract.py
@@ -17,19 +17,25 @@ class TestLibYr(unittest.TestCase):
         self.weather_data = weather_data
         start = datetime(2018, 3, 18, 19)
         end = datetime(2018, 3, 18, 23)
-        self.weather_extract = YrExtract(self.weather_data, start, end)
+        self.weather_extract = YrExtract(self.weather_data.forecast(as_json=False), start, end)
 
     def test_extract_min_temperature(self):
-        self.assertEqual(self.weather_extract.min_temperature, 3)
+        self.assertEqual(self.weather_extract.min_temperature, 3.)
 
     def test_extract_max_temperature(self):
-        self.assertEqual(self.weather_extract.max_temperature, 4)
+        self.assertEqual(self.weather_extract.max_temperature, 4.)
 
     def test_extract_min_precipitation(self):
-        self.assertEqual(self.weather_extract.min_precipitation, 0.5 + 0.7 + 0.8)
+        self.assertEqual(self.weather_extract.min_precipitation, 0.4 + 0.5 + 0.7 + 0.8)
 
     def test_extract_max_precipitation(self):
-        self.assertEqual(self.weather_extract.max_precipitation, 1.2 + 2.6 + 2.2)
+        self.assertEqual(self.weather_extract.max_precipitation, 1.0 + 2.1 + 2.6 + 2.2)
+
+    def test_extract_min_wind(self):
+        self.assertEqual(self.weather_extract.min_wind_speed, 3.6)
+
+    def test_extract_max_wind(self):
+        self.assertEqual(self.weather_extract.max_wind_speed, 9.8)
 
 
 if __name__ == '__main__':
--- yr/yrextract.py
+++ yr/yrextract.py
@@ -1,4 +1,78 @@
-
+from datetime import datetime
+from collections import OrderedDict
 
 class YrExtract:
-    pass
+
+    def __init__(self, forecast: OrderedDict, date_start: datetime, date_end: datetime):
+        self._forecast = forecast
+        self._start = date_start
+        self._end = date_end
+
+    def _get_extract(self):
+        out = []
+        for line in self._forecast:
+            from_time = datetime.strptime(line['@from'], "%Y-%m-%dT%H:%M:%S")
+            if from_time < self._start:
+                continue
+
+            to_time = datetime.strptime(line['@to'], "%Y-%m-%dT%H:%M:%S")
+            if to_time > self._end:
+                break
+
+            out.append(line)
+        return out
+
+    @property
+    def min_temperature(self) -> float:
+        min_temperature = 1000
+        for line in self._get_extract():
+            temperature = float(line['temperature']['@value'])
+            min_temperature = min(min_temperature, temperature)
+        return min_temperature
+
+    @property
+    def max_temperature(self) -> float:
+        max_temperature = -1000
+        for line in self._get_extract():
+            temperature = float(line['temperature']['@value'])
+            max_temperature = max(max_temperature, temperature)
+        return max_temperature
+
+    @property
+    def min_wind_speed(self) -> float:
+        min_v = 1000
+        for line in self._get_extract():
+            v = float(line['windSpeed']['@mps'])
+            min_v = min(min_v, v)
+        return min_v
+
+
+    @property
+    def max_wind_speed(self) -> float:
+        max_v = -1000
+        for line in self._get_extract():
+            v = float(line['windSpeed']['@mps'])
+            max_v = max(max_v, v)
+        return max_v
+
+    @property
+    def max_temperature(self) -> float:
+        max_temperature = -1000
+        for line in self._get_extract():
+            temperature = float(line['temperature']['@value'])
+            max_temperature = max(max_temperature, temperature)
+        return max_temperature
+
+    @property
+    def min_precipitation(self) -> float:
+        sum = 0
+        for line in self._get_extract():
+            sum += float(line['precipitation']['@minvalue'])
+        return sum
+
+    @property
+    def max_precipitation(self) -> float:
+        sum = 0
+        for line in self._get_extract():
+            sum += float(line['precipitation']['@maxvalue'])
+        return sum
